9a0e58f7df1b26d780332b7ec476212e8b6233cc
张新 <nobugboy@163.com>
2024-05-05 20:32:14 +0800

    代码优化

--- neo-chat-trigger/src/main/java/com/neo/trigger/job/NoPayNotifyOrderJob.java
+++ neo-chat-trigger/src/main/java/com/neo/trigger/job/NoPayNotifyOrderJob.java
@@ -62,14 +62,14 @@ public class NoPayNotifyOrderJob {
                     if (response.isSuccess()) {
                         String body = response.getBody();
                         JSONObject trade = JSONObject.parseObject(body);
-                        String tradeStatus = trade.getString("trade_status");
+
+                        JSONObject alipayTradeQueryResponse = trade.getJSONObject("alipay_trade_query_response");
+
+                        String tradeStatus = alipayTradeQueryResponse.getString("trade_status");
                         if (!"TRADE_SUCCESS".equals(tradeStatus)) {
                             log.info("定时任务，订单支付状态更新，当前订单未支付 orderId is {}, 订单支付状态为{}", payOrderEntity.getOrderId(), tradeStatus);
                             continue;
                         }
-
-                        JSONObject alipayTradeQueryResponse = trade.getJSONObject("alipay_trade_query_response");
-
                         boolean isSuccess = orderService.changeOrderPaySuccess(payOrderEntity.getOrderId(), ""
                                 , alipayTradeQueryResponse.getBigDecimal("total_amount")
                                 , LocalDateTime.parse(alipayTradeQueryResponse.getString("send_pay_date"), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
